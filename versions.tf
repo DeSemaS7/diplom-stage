1f4e8e986a1ccbab2372283245588620e70e1eef
DeSemaS7 <desema.s7@gmail.com>
2022-02-03 17:55:20 +0300

    vers

--- versions.tf
+++ versions.tf
@@ -6,21 +6,3 @@ terraform {
     }
   }
 }
-
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "neto-diplom"
-
-    workspaces {
-      prefix = "diplom-"
-    }
-  }
-}
-
-provider "yandex" {
-  token     = "<OAuth>"
-  cloud_id  = "b1ge9kc6o4mjqkq8g442"
-
-}
-
