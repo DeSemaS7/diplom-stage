9a79818799d60bcd4e43945c0f73808732f09a68
DeSemaS7 <desema.s7@gmail.com>
2022-02-03 17:41:39 +0300

    vpc

--- vpc.tf
+++ vpc.tf
@@ -2,19 +2,19 @@ resource "yandex_vpc_network" "terra_network" {
   name = "network1"
 }
 
-resource "yandex_vpc_subnet1" "public-a" {
+resource "yandex_vpc_subnet" "public-a" {
   name           = "public"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.terra_network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-resource "yandex_vpc_subnet1" "public-b" {
+resource "yandex_vpc_subnet" "public-b" {
   name           = "public"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.terra_network.id
   v4_cidr_blocks = ["192.168.11.0/24"]
 }
-resource "yandex_vpc_subnet1" "public-c" {
+resource "yandex_vpc_subnet" "public-c" {
   name           = "public"
   zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.terra_network.id
