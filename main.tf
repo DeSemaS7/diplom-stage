374bcaca79ba9a981e215bf43f4b2efa9bd902ed
DeSemaS7 <desema.s7@gmail.com>
2022-02-04 14:12:29 +0300

    key

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 
 provider "yandex" {
-  token     = var.yc-token
+  service_account_key_file = "./terracloud.json"
   folder_id = "b1grcnk7hrc2350cime9"
   cloud_id  = "b1ge9kc6o4mjqkq8g442"
 
