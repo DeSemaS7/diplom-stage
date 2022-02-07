abf22dc128d5d6fca78f22036f2953af688c67c5
DeSemaS7 <desema.s7@gmail.com>
2022-02-07 15:55:37 +0300

    k8s

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "master_ext_address" {
-  value = yandex_kubernetes_cluster.first-cluster.master.external_v4_address
+  value = yandex_kubernetes_cluster.first-cluster.master[0].external_v4_address
 }
 output "master_ext_endpoint" {
-  value = yandex_kubernetes_cluster.first-cluster.master.external_v4_endpoint
+  value = yandex_kubernetes_cluster.first-cluster.master[0].external_v4_endpoint
 }
