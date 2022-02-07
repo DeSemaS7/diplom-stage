856a6971fd4e752d52db480c5d8d77f6095f2fc6
DeSemaS7 <desema.s7@gmail.com>
2022-02-07 12:36:20 +0300

    workspaces

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 
 provider "yandex" {
-  service_account_key_file = "./key.json"
+  service_account_key_file = "./terracloud.json"
   folder_id = "b1grcnk7hrc2350cime9"
   cloud_id  = "b1ge9kc6o4mjqkq8g442"
 
