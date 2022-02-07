2031c894c6afa55a5e494354b792fa83846b53df
DeSemaS7 <desema.s7@gmail.com>
2022-02-07 11:49:39 +0300

    worksapces

--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,22 @@
 resource "yandex_vpc_network" "terra_network" {
-  name = "network1"
+  name = "network-${terraform.workspace}"
 }
 
-resource "yandex_vpc_subnet" "public-a" {
-  name           = "public-a"
+resource "yandex_vpc_subnet" "public-a-${terraform.workspace}" {
+  name           = "public-a-${terraform.workspace}"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.terra_network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
+
 }
-resource "yandex_vpc_subnet" "public-b" {
-  name           = "public-b"
+resource "yandex_vpc_subnet" "public-b-${terraform.workspace}" {
+  name           = "public-b-${terraform.workspace}"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.terra_network.id
   v4_cidr_blocks = ["192.168.11.0/24"]
 }
-resource "yandex_vpc_subnet" "public-c-stage" {
-  name           = "public-d-${terraform.workspace}"
+resource "yandex_vpc_subnet" "public-c-${terraform.workspace}" {
+  name           = "public-c-${terraform.workspace}"
   zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.terra_network.id
   v4_cidr_blocks = ["192.168.12.0/24"]
