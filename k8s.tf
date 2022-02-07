bb5eb8e3809fccc86e396ed5a428e37d5b8d027f
DeSemaS7 <desema.s7@gmail.com>
2022-02-07 16:40:45 +0300

    k8s

--- k8s.tf
+++ k8s.tf
@@ -52,3 +52,49 @@ resource "yandex_kubernetes_cluster" "first-cluster" {
   release_channel = "STABLE"
   depends_on = [yandex_resourcemanager_folder_iam_member.k8s-editor]
 }
+resource "yandex_kubernetes_node_group" "k8s-nodes" {
+  cluster_id  = "${yandex_kubernetes_cluster.first-cluster.id}"
+  name        = "k8s-nodes"
+
+  instance_template {
+    platform_id = "standard-v2"
+
+    network_interface {
+      nat                = true
+      subnet_ids         = ["${yandex_vpc_subnet.network-a.id}"]
+    }
+
+    resources {
+      memory = 2
+      cores  = 2
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 64
+    }
+
+    scheduling_policy {
+      preemptible = false
+    }
+
+    container_runtime {
+      type = "containerd"
+    }
+  }
+
+  scale_policy {
+    auto_scale {
+      initial = 1
+      max = 4
+      min = 1
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = "ru-central1-a"
+    }
+  }
+
+}
