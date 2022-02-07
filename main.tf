c9469844f0d7798438b105bc2db5e4f4d281d82e
DeSemaS7 <desema.s7@gmail.com>
2022-02-07 12:33:08 +0300

    workspaces

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 
 provider "yandex" {
-  service_account_key_file = "${terraform.workspace} == stage ? ./terracloud.json : ./keytest.json"
+  service_account_key_file = "./key.json"
   folder_id = "b1grcnk7hrc2350cime9"
   cloud_id  = "b1ge9kc6o4mjqkq8g442"
 
