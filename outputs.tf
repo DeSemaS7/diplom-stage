5711baed2d8fd992b0569e7140657d23b2c06509
DeSemaS7 <desema.s7@gmail.com>
2022-02-09 13:05:51 +0300

    nginx

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,6 @@
 output "master_ext_address" {
   value = yandex_kubernetes_cluster.first-cluster.master[0].external_v4_address
 }
-output "registry_id" {
-  value = yandex_container_registry.my-registry.id
+output "repo_name" {
+  value = yandex_container_repository.mynginx.name
 }
-
-
--- registry.tf
+++ registry.tf
@@ -1,9 +1,13 @@
 resource "yandex_container_registry" "my-registry" {
   name      = "my-registry"
 }
-resource "yandex_container_registry_iam_binding" "editor" {
-  registry_id = yandex_container_registry.my-registry.id
-  role        = "editor"
+resource yandex_container_repository mynginx {
+  name      = "${yandex_container_registry.my-registry.id}/mynginx"
+  depends_on = [yandex_container_registry.my-registry]
+}
+resource "yandex_container_repository_iam_binding" "pusher" {
+  repository_id = yandex_container_repository.mynginx.id
+  role        = "container-registry.images.pusher"
 
   members = [
     "system:allUsers",
