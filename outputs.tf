48b6170fd550b3747168c53aff5e22a3dd4e2a05
DeSemaS7 <desema.s7@gmail.com>
2022-02-20 17:32:14 +0300

    readme.md

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "master_ext_address" {
-  value = yandex_kubernetes_cluster.first-cluster.master[0].external_v4_address
+  value = yandex_kubernetes_cluster.stage-cluster.master[0].external_v4_address
 }
 output "repo_name" {
   value = yandex_container_repository.myapp.name
